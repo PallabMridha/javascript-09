caaba78819506d3cd249b7e94301a62baf9708bd
eCommerce-Website~ <pallabmridha@gmail.com>
2025-05-27 12:53:10 +0600

    noton kiso

--- js/home.js
+++ js/home.js
@@ -3,6 +3,14 @@ let addMoneyPin = document.getElementById("addMoneyPin")
 let moneyBtn = document.getElementById("moneyBtn")
 let currentBlance = document.getElementById("currentBlance")
 let logout = document.getElementById("logout")
+let addMoneyPart = document.getElementById("addMoneyPart")
+let addMoneyBtn = document.getElementById("addMoneyBtn")
+
+
+addMoneyBtn.addEventListener("click", ()=>{
+   addMoneyPart.style.display = "block" 
+   cashOutPart.style.display = "none" 
+})
 
 
 // ============
@@ -28,8 +36,44 @@ moneyBtn.addEventListener("click", () => {
 })
 
 // ========== end =========
-let outBtn = document.getElementById("outBtn")
 
-outBtn.addEventListener("click" , function () {
-    window.location.href = "./cashout.html"
+
+
+
+let outBtn = document.getElementById("outBtn")
+let cashOutAmount = document.getElementById("cashOutAmount")
+let cashOutPin = document.getElementById("cashOutPin")
+let cashOutBtn = document.getElementById("cashOutBtn")
+let cashOutPart = document.getElementById("cashOutPart")
+
+
+outBtn.addEventListener("click", ()=>{
+   cashOutPart.style.display = "block" 
+   addMoneyPart.style.display = "none" 
 })
+
+
+
+cashOutBtn.addEventListener("click", function () {
+
+    
+    if (cashOutPin.value == 2525) {
+       let cashOutFinalTk = parseFloat(cashOutAmount.value )
+       let agerTakaCashOut = parseFloat(currentBlance.innerHTML)
+       let finalCashOutAmmount = agerTakaCashOut - cashOutFinalTk
+       
+       currentBlance.innerHTML = finalCashOutAmmount
+       
+    }
+    else{
+        alert("Cash Out Pin Vul")
+    }
+})
+
+
+
+
+// ======== end =======
+
+
+
